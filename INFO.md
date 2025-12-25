ab7c678d85a0b6e2d7dd86cfa7f4ac52aff9433f
Marshall Akpan <uimarshall@gmail.com>
2025-12-25 11:15:52 +0000

    Add project setup files and configure Next.js.
    Setup initial project structure with Next.js, Biome,
    and necessary configurations.

---
+++ INFO.md
@@ -0,0 +1,3 @@
+> layout.tsx
+
+The `layout.tsx` file defines a layout that is applied to every page in the application. It contains the HTML structure and wraps the children components that will be rendered on each page.
---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+// import { dirname } from 'node:path';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // turbopack: {
+  //   root: dirname(__dirname),
+  // },
+};
+
+export default nextConfig;
