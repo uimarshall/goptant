6a9f478057cc1aefd41a78f9da1a2d4b45970243
Marshall Akpan <uimarshall@gmail.com>
2026-02-04 06:02:25 +0000

    Create-Protected-Server-Actions
    In this commit, I've updated the blog editing and article upload functionalities to ensure that only authenticated users can perform these actions. The changes include modifications to the article editing page, the new article creation page, and the upload action handler. Now, if a user attempts to access these features without being logged in, they will be redirected to the login page. This enhancement improves the security of our application by restricting sensitive operations to authorized users only.

--- src/app/blog/edit/new/page.tsx
+++ src/app/blog/edit/new/page.tsx
@@ -1,8 +1,8 @@
-import WikiEditor from '@/components/BlogEditor';
-import { stackServerApp } from '@/stack/server';
+import WikiEditor from "@/components/BlogEditor";
+import { stackServerApp } from "@/stack/server";
 
 export default async function NewArticlePage() {
   // Ensure the user is logged in before allowing them to create a new article.Else, redirect to the login page.
-  await stackServerApp.getUser({ or: 'redirect' });
+  await stackServerApp.getUser({ or: "redirect" });
   return <WikiEditor isEditing={false} />;
 }
